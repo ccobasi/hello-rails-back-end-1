1427486fa23a6d18aa78f1d9f182f018d55593af
Francis Uloko <francisuloko@yahoo.com>
2021-10-13 22:37:49 +0100

    Add greetings endpoint at /greetings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Greeting.destroy_all
+
+greeting = Greeting.create!(
+  [
+    {message: 'Hello'},
+    {message: 'Hola'},
+    {message: 'Allo'},
+    {message: 'Ola'},
+    {message: '여보세요'}
+  ]
+)
